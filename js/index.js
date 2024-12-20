5d49b7ef8f59dd101603df3ec64dba5eb947277b
jbrunops <jbrunops@outlook.com>
2024-12-20 18:50:29 -0300

    Primeira parte do JavaScript adicionada

--- js/index.js
+++ js/index.js
@@ -12,4 +12,26 @@ const addNewItem = () =>{
     error.classList.replace("delet", "error");
     return;
   }
+
+  const li = document.createElement("li");
+
+  li.classList.add("item");
+  li.style.marginBottom = `0.75rem`;
+
+  const div = document.createElement("div");
+  const checkbox = document.createElement("input");
+
+  ckeckbox.setAttribute("tyle", "checkbox");
+
+  const span = document.createElement("span");
+  span.textContent = input.value;
+
+  const del = document.createElement("button");
+  del.innerHTML = `<img src="./icons/Frame-3.svg" alt="">`;
+
+  div.append(checkbox, span);
+  li.prepend(div, del);
+  ul.append(li);
+
+  input.value = "";
 }
